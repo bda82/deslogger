a1eba1ec112a444dc5f4f416d5041004b62dc011
Dmitry <bda82@mail.ru>
2021-08-12 08:04:45 +0300

    move config to main file

--- desire_logger/desire_logger.py
+++ desire_logger/desire_logger.py
@@ -7,7 +7,67 @@ import json
 from logging.handlers import BufferingHandler
 import traceback
 
-from config.config import Config
+import os
+
+
+class ClickHouseConfig:
+    host = ''
+    port = ''
+    user = ''
+    password = ''
+    db = ''
+    secure = False
+    verify = False
+    compression = True
+
+
+class Config:
+
+    def __init__(self):
+        self.use_db = True
+        self.use_stdout = True
+        self.use_separate_files = True
+        self.ch_conn = 'http://localhost:8123'
+        self.ch_table = 'desire_logs'
+        self.buffer_capacity = 1
+        self.build_in_keys_to_ch = ['message',
+                               'levelname',
+                               'filename',
+                               'module',
+                               'lineno,',
+                               'exc_info',
+                               'created',
+                               'msecs',
+                               'relativeCreated',
+                               'asctime']
+        self.build_in_log_keys = ['name',
+                             'msg',
+                             'args',
+                             'levelname',
+                             'levelno',
+                             'pathname',
+                             'filename',
+                             'module',
+                             'exc_info',
+                             'exc_text',
+                             'stack_info',
+                             'lineno',
+                             'funcName',
+                             'created',
+                             'msecs',
+                             'relativeCreated',
+                             'thread',
+                             'threadName',
+                             'processName',
+                             'process']
+
+        self.clickhouse = ClickHouseConfig()
+        self.clickhouse.host = os.environ.get('CLICKHOUSE_HOST', 'localhost')
+        self.clickhouse.port = os.environ.get('CLICKHOUSE_PORT', 8123)
+        self.clickhouse.db = os.environ.get('CLICKHOUSE_DB', 'clickhouse')
+        self.clickhouse.user = os.environ.get('CLICKHOUSE_USER', 'root')
+        self.clickhouse.password = os.environ.get('CLICKHOUSE_PASSWORD', 'password')
+
 
 
 class CallStackFormatter(logging.Formatter):
